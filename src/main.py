25ca5639fbe5f0a3133b359af0027efe1e1e7cca
Junho Han <jyuno426@kaist.ac.kr>
2020-06-19 18:25:10 +0900

    random graph embedding

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ from util import load_dataset, make_graph
 
 
 if __name__ == '__main__':
-    n, paper_author_data, public_true_data, public_false_data, private_data = load_dataset()
-    graph = make_graph(paper_author_data + public_true_data)
-    build_node_embedding(graph, dimensions=10, p=1, q=0.5,
-                         walk_length=10, num_walks=50, workers=4)
+    # n, paper_author_data, public_true_data, public_false_data, private_data = load_dataset()
+    # graph = make_graph(paper_author_data + public_true_data)
+
+    import networkx as nx
+    graph = nx.fast_gnp_random_graph(n=100, p=0.5)
+    build_node_embedding(graph, dimensions=10, p=1, q=2,
+                         walk_length=10, num_walks=50, workers=1)
--- src/node_embedding.py
+++ src/node_embedding.py
@@ -13,13 +13,13 @@ def build_node_embedding(graph, dimensions=30, p=1, q=1, walk_length=10, num_wal
                         temp_folder=temp_folder)  # Use temp_folder for big graphs
 
     # Embed nodes
-    model = node2vec.fit(
-        window=10, min_count=1, batch_words=4
-    )  # Any keywords acceptable by gensim.Word2Vec can be passed, `diemnsions` and `workers` are automatically passed (from the Node2Vec constructor)
+    # Any keywords acceptable by gensim.Word2Vec can be passed,
+    # `diemnsions` and `workers` are automatically passed (from the Node2Vec constructor)
+    model = node2vec.fit(window=10, min_count=1, batch_words=4)
 
     # Save embeddings for later use
     model.wv.save_word2vec_format(
-        "./output/node_embedding_p{}_q{}".format(p, q))
+        "../output/node_embedding_p{}_q{}".format(p, q))
 
     # Save model for later use
-    model.save("./model/node_embedding")
+    # model.save("../model/node_embedding")
